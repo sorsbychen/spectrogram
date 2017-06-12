7f459f0eb6f04faf599cbdfd5a4d06b5ad04cc57
alweis <xuewei.x@gmail.com>
2017-06-12 21:30:34 +0100

    'Wei-first-version-finalized'

--- lambda_code/lambda.py
+++ lambda_code/lambda.py
@@ -48,12 +48,12 @@ def get_welcome_response():
     add those here
     """
 
-    session_attributes = {}
+    session_attributes = {'status':0,'usr':'xxxx','order':'xxxx'}
     card_title = "Welcome"
     speech_output = "Welcome to the BP restaurant. My name is Alexa. "\
-    				"I am your waiter today. Let me tell you about our specials today."\
-    				" We have a miso-glazed Chilean Sea Bass, with a side of mashed sweet potatoes, and sauteed spinach."\
-    				"You can ask me questions about the menu. To order your food, you just need to say, Alexa, start ordering."
+                    "I am your waiter today. Let me tell you about our specials today. "\
+                    " We have a miso-glazed Chilean Sea Bass, with a side of mashed sweet potatoes, and sauteed spinach. "\
+                    "You can ask me questions about the menu. To order your food, you just need to say, Alexa, start ordering."
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
     reprompt_text = speech_output
@@ -79,86 +79,107 @@ def create_favorite_color_attributes(favorite_color):
 def set_usr_in_session(intent, session):
 
     card_title = intent['name']
-    session_attributes = {}
+    session_attributes = session['attributes']
+    session_attributes['status'] = 2
+
     should_end_session = False
 
-    usr_name = intent['slots']['name']['value']
-    speech_output = "Hello" + \
-                        usr_name + \
-                        ". What can get for you?" 
+    usr_name_str = intent['slots']['Name']['value']
+    speech_output = "Hello," + usr_name_str + ". What can get for you?" 
+    session_attributes['usr'] = usr_name_str
 
     reprompt_text = speech_output
 
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
-
-def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
-    """
+def set_order_in_session(intent, session):
 
     card_title = intent['name']
-    session_attributes = {}
+    session_attributes = session['attributes']
+    session_attributes['status'] = 3
+
     should_end_session = False
 
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                        favorite_color + \
-                        ". You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, " \
-                        "my favorite color is red."
+    order_name_str = intent['slots']['Food']['value']
+    speech_output = "Sure, a " + order_name_str + " for " + session_attributes['usr']
+    if 'steak' in order_name_str or 'Steak' in order_name_str:
+        speech_output = speech_output + '. Hi, '+ session_attributes['usr'] + ', how do you like your steak done?'
+        session_attributes['status'] = -3        
+    session_attributes['order'] = order_name_str
+
+    reprompt_text = 'sorry I cannot recognize you.'
+
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
+def set_steak_in_session(intent, session):
+
+    card_title = intent['name']
+    session_attributes = session['attributes']
+    session_attributes['status'] = 3
+
+    should_end_session = False
+
+    stk_name_str = intent['slots']['SteakValue']['value']
+    speech_output = "Sure, " + stk_name_str + " it is." 
+
+    reprompt_text = 'sorry I cannot recognize you.'
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
 
 def start_order_session(intent, session):
     """ Start ordering
     """
 
     card_title = intent['name']
-    session_attributes = {}
+    session_attributes = session['attributes']
+    session_attributes['status'] = 1
+
     should_end_session = False
 
 
-    speech_output = "May I have your name please?"
-    reprompt_text = 'Sorry, I cannot understand your speech'
+    speech_output = "Thank you. May I have your name please?"
+    reprompt_text = 'Sorry, I cannot understand your speech.'
 
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
 
+def fail_session(intent, session):
+
+    card_title = intent['name']
+
+    should_end_session = False
 
 
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
+    speech_output = 'Sorry, I cannot understand your speech.'
+    reprompt_text = 'Sorry, I cannot understand your speech.'
 
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
-
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
     return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
+        card_title, speech_output, reprompt_text, should_end_session))
 
+def reply_query_session(intent, session):
+    """ Start ordering
+    """
+
+    card_title = intent['name']
+    session_attributes = session['attributes']
+
+    should_end_session = False
+
+    order_name_str = intent['slots']['Food']['value']
+
+    if order_name_str == 'mushroom pasta':
+        speech_output = 'Calories per serving of mushroom pasta: 200 calories of Pasta, 65 calories of Cream of Mushroom Soup. In total 265 calories.'
+    else:
+        speech_output = 'Sorry, I have no idea.'
+
+    reprompt_text = 'Sorry, I cannot understand your speech.'
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
 
 # --------------- Events ------------------
 
@@ -167,6 +188,20 @@ def on_session_started(session_started_request, session):
 
     print("on_session_started requestId=" + session_started_request['requestId']
           + ", sessionId=" + session['sessionId'])
+
+
+def handle_session_end_request(intent, session):
+    card_title = "Session Ended"
+    speech_output = "Good choices. Your food will be ready in 10 minutes."
+    session_attributes = session['attributes']
+    session_attributes['status'] = 0
+
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
+
 
 
 def on_launch(launch_request, session):
@@ -190,13 +225,34 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "StartOrderIntent":
-        return start_order_session(intent, session)
-    elif intent_name == "MyNameIsIntent":
-    	return set_usr_in_session(intent, session)
-    else:
-        raise ValueError("Invalid intent")
+    session_attributes = session['attributes']
+    try:
+        if  intent_name == "StartOrderIntent":
+            return start_order_session(intent, session)
 
+        if  intent_name == "QueryIntent":
+            return reply_query_session(intent, session)
+        if  intent_name == "FinishIntent":
+            return handle_session_end_request(intent, session)
+
+        if session_attributes['status'] == 1: # after start ordering
+            if intent_name == 'StartOrderIntent':
+                return start_order_session(intent, session) #repeat if start ordering is asked again
+            elif intent_name == "MyNameIsIntent":
+                return set_usr_in_session(intent, session) #after my name is ..., then status is 2
+        if session_attributes['status'] == 2:  #after my name is ...
+            if intent_name == "MyNameIsIntent":
+                return set_usr_in_session(intent, session) #repeat if new name
+            elif intent_name == "MakeOrderIntent":
+                return set_order_in_session(intent,session)
+        if session_attributes['status'] == 3:  #after my name is ...
+            if intent_name == "MakeOrderIntent":
+                return set_order_in_session(intent,session)
+        if session_attributes['status'] == -3:
+            if intent_name == 'SteakIntent':
+                return set_steak_in_session(intent,session)
+    except ValueError:
+        return fail_session(intent,session)
 
 def on_session_ended(session_ended_request, session):
     """ Called when the user ends the session.
@@ -214,25 +270,30 @@ def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
     etc.) The JSON body of the request is provided in the event parameter.
     """
-    print("event.session.application.applicationId=" +
-          event['session']['application']['applicationId'])
+    try:
+        print("event.session.application.applicationId=" +
+              event['session']['application']['applicationId'])
 
-    """
-    Uncomment this if statement and populate with your skill's application ID to
-    prevent someone else from configuring a skill that sends requests to this
-    function.
-    """
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
+        """
+        Uncomment this if statement and populate with your skill's application ID to
+        prevent someone else from configuring a skill that sends requests to this
+        function.
+        """
+        # if (event['session']['application']['applicationId'] !=
+        #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+        #     raise ValueError("Invalid Application ID")
 
-    if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']},
-                           event['session'])
+        if event['session']['new']:
+            on_session_started({'requestId': event['request']['requestId']},
+                               event['session'])
 
-    if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
-    elif event['request']['type'] == "IntentRequest":
-        return on_intent(event['request'], event['session'])
-    elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])
+        if event['request']['type'] == "LaunchRequest":
+            response_r = on_launch(event['request'], event['session'])
+            return response_r
+        elif event['request']['type'] == "IntentRequest":
+            response_r = on_intent(event['request'], event['session'])
+            return response_r
+        elif event['request']['type'] == "SessionEndedRequest":
+            return on_session_ended(event['request'], event['session'])
+    except ValueError:
+        return fail_session(event['request'], event['session'])
